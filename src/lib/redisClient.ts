b8ccfd10141b117f07706a384699a5166b118af4
Chandan <cgupta7510@gmail.com>
2025-11-03 19:56:59 +0530

    redis multiple entry point error to be fixed

--- src/lib/redisClient.ts
+++ src/lib/redisClient.ts
@@ -1,41 +1,54 @@
-import { createClient, RedisClientType } from "redis";
+import { createClient, RedisClientType } from "redis"
 
-declare global {
-  var redisPromise: Promise<RedisClientType> | undefined
-}
+let redisClient: RedisClientType | null = null
 
-const config = {
+const redisConfig = {
   username: process.env.REDIS_USERNAME,
   password: process.env.REDIS_PASSWORD,
   socket: {
-    host: process.env.REDIS_HOST,
-    port: parseInt(process.env.REDIS_PORT!, 10),
+    host: process.env.REDIS_HOST || "localhost",
+    port: parseInt(process.env.REDIS_PORT || "6379", 10),
     reconnectStrategy: (retries: number) => Math.min(retries * 50, 5000)
   },
+  maxRetriesPerRequest: 3,
+  connectTimeout: 5000,
+  lazyConnect: true,
 }
 
-async function  createConnetionProise(): Promise<RedisClientType> {
-  console.log("Creating new connection and client promise");
-  const redisClient: RedisClientType = createClient(config)
+async function startRedisConnection() {
+  if (!redisClient) {
+    try {
+      redisClient = createClient(redisConfig)
 
-  redisClient.on("connect", () => console.log("Redis Connected"))
-  redisClient.on("error", (err) => console.error("Redis error", err))
-  redisClient.on("reconnecting", () => console.error("Redis reconnecting"))
+      redisClient.on("connect", () => {
+        console.log("Redis connected");
+      })
+      redisClient.on("error", (err) => {
+        console.log("Redis connection error", err)
+      })
+      await redisClient.connect();
 
+    } catch (error) {
+      console.error("Redis connection failed", error);
+      throw new Error("Redis connection failed.")
+    }
+  }
+}
+
+export async function getRedisClient():Promise<RedisClientType> {
+  if(!redisClient){
+    throw new Error("Redis client not started.")
+  }
   return redisClient
-    .connect()
-    .then(() => {
-      return redisClient
-    })
-    .catch((err) => {
-      console.error("error connecting redis", err);
-      global.redisPromise = undefined
-      throw err
-    })
 }
 
+(async()=>{
+  try {
+    await startRedisConnection()
+  } catch (error) {
+      console.error("Redis start error.", error)
+  }
+})()
 
-const connectionPromise: Promise<RedisClientType> = global.redisPromise ?? (global.redisPromise = createConnetionProise())
-export async function getRedisClient(): Promise<RedisClientType> {
-  return connectionPromise
-}
+
+
